0b51aee2519ee444e0c716d326258050b529c11a
UtsavSaliya <utsavpatel7273@gmail.com>
2024-05-21 11:04:37 +0530

    Area.java

--- hw1/src/hw1/Area.java
+++ hw1/src/hw1/Area.java
@@ -2,10 +2,17 @@ package hw1;
 
 public class Area {
     private double length, width;
+    
+    // Default constructor
+    public Area() {
+        this.length = 0.0;
+        this.width = 0.0;
+    }
     public void setDim(double length, double width) {
         this.length = length;
         this.width = width;
     }
+
     public double getArea() {
         return length * width;
     }
